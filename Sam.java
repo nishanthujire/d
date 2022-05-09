8ad5a8fe2f016fdac1b4b4198f3848db9c4c595c
Nishanth <67428241+nishanthujire@users.noreply.github.com>
2022-05-09 20:16:29 +0530

    gg

--- Sam.java
+++ Sam.java
@@ -4,6 +4,9 @@ class Sam {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, nishanth!"); 
+        System.out.println("Hello, sam!"); 
+        
+        
         
     }
 }
